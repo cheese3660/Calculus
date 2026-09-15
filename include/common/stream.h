6484dfdac46e6f8f14337b570191db93cb5682cc
Infinity Irrealis <cheese.queen.lexi@gmail.com>
2026-09-15 08:55:42 -0400

    Full enough stream implementation

--- include/common/stream.h
+++ include/common/stream.h
@@ -3,7 +3,7 @@
  *  stream.h
  *  author: Lexi Allen
  *  license: MIT
- *  last updated: 9/14/2026
+ *  last updated: 9/15/2026
  *
  *  Generic data stream library, can be used to read/write data from a "stream"
  *  of data, like a file or memory
@@ -22,19 +22,23 @@ struct stream_header;
 
 struct stream_info
 {
-    ssize_t (*read)(struct stream_header *stream, void *buffer, size_t buffer_size);
-    ssize_t (*write)(struct stream_header *stream, void *buffer, size_t buffer_size);
-    void (*flush)(struct stream_header *stream);
-    void (*close)(struct stream_header *stream);
-    const char *(*error)(struct stream_header *stream);
+    ssize_t (*read)(struct stream_header *stream, void *buffer, size_t buffer_size)
+        __attribute__((access(write_only, 2, 3), nonnull(1, 2)));
+    ssize_t (*write)(struct stream_header *stream, const void *buffer, size_t buffer_size)
+        __attribute__((access(read_only, 2, 3), nonnull(1, 2)));
+    void (*flush)(struct stream_header *stream)
+        __attribute__((nonnull(1)));
+    void (*close)(struct stream_header *stream)
+        __attribute__((nonnull(1)));
+    const char *(*error)(const struct stream_header *stream)
+        __attribute__((access(read_only, 1), nonnull(1), returns_nonnull));
 };
 
 enum stream_status
 {
     STREAM_OK,
     STREAM_END,
-    STREAM_ERRORED,
-    STREAM_TRUNCATED
+    STREAM_ERRORED
 };
 
 typedef struct stream_header
@@ -43,47 +47,56 @@ typedef struct stream_header
     enum stream_status status;
 } *stream_t;
 
+typedef const struct stream_header *const_stream_t;
+
+// Forward declare this for the malloc attributes
+void sm_close(stream_t stream);
+
 /// @brief Wrap a stream around a file
 /// @param file The file to wrap
 /// @return A stream for reading/writing from the file
-stream_t sm_file(FILE *file) __attribute__((nonnull(1)));
+stream_t sm_file(FILE *file) __attribute__((malloc, malloc(sm_close, 1), access(read_write, 1), nonnull(1), returns_nonnull, warn_unused_result));
 
 /// @brief Create a stream for writing to memory
 /// @param buffer A pointer to a buffer that will be allocated with malloc, if set, it already has to be mallocated
 /// @param buffer_size A pointer the amount of data that has been written to the stream, if set going in it is the initial capacity of the buffer
 /// @return A stream for writing to memory that will be pointed to by buffer, and sized by buffer size
 /// @warning Closing the stream will free the memory buffer that backs it, memcpy the data before closing it if you need to keep it
-stream_t sm_memwrite(void **buffer, size_t *buffer_size) __attribute__((access(read_write, 1), access(read_write, 2), nonnull(1, 2)));
+stream_t sm_memwrite(void **buffer, size_t *buffer_size) __attribute__((malloc, malloc(sm_close, 1), access(read_write, 1), access(read_write, 2), nonnull(1, 2), returns_nonnull, warn_unused_result));
 
 /// @brief Create a stream for reading from (fixed size) memory
 /// @param buffer The buffer being read from
 /// @param buffer_size How much data is contained in the buffer
 /// @return A stream for reading from the buffer
-stream_t sm_memread(void *buffer, size_t buffer_size) __attribute__((access(read_only, 1, 2), nonnull(1)));
+stream_t sm_memread(const void *buffer, size_t buffer_size) __attribute__((malloc, malloc(sm_close, 1), access(read_only, 1, 2), nonnull(1), returns_nonnull, warn_unused_result));
 
 /// @brief Read from a stream
 /// @param stream The stream to read from
 /// @param buffer The buffer to read into
 /// @param buffer_size The amount of data to read
 /// @return The amount of data read, -1 if failed to read
-ssize_t sm_read(stream_t stream, void *buffer, size_t buffer_size) __attribute__((access(write_only, 2, 3), nonnull(1,2)));
+ssize_t sm_read(stream_t stream, void *buffer, size_t buffer_size) __attribute__((access(read_write, 1), access(write_only, 2, 3), nonnull(1, 2), warn_unused_result));
 
 /// @brief Write to a stream
 /// @param stream The stream to write to
 /// @param buffer The buffer to write
 /// @param buffer_size The amount of data to write
 /// @return The amount of data written, -1 if failed to write
-ssize_t sm_write(stream_t stream, void *buffer, size_t buffer_size) __attribute__((access(read_only, 2, 3), nonnull(1,2)));
+ssize_t sm_write(stream_t stream, const void *buffer, size_t buffer_size) __attribute__((access(read_write, 1), access(read_only, 2, 3), nonnull(1, 2)));
 
 /// @brief Flush a stream
 /// @param stream The stream to flush
 /// @return The status of the stream after the flush
-enum stream_status sm_flush(stream_t stream) __attribute__((nonnull(1)));
+enum stream_status sm_flush(stream_t stream) __attribute__((access(read_write, 1), nonnull(1)));
 
 /// @brief Close a stream
 /// @param stream The stream to close, this also free()'s the stream
-/// @return The status of the stream after the close
-enum stream_status sm_close(stream_t stream) __attribute__((nonnull(1)));
+void sm_close(stream_t stream) __attribute__((access(read_write, 1), nonnull(1)));
+
+/// @brief Get the current error for a stream
+/// @param stream The stream to get the error from
+/// @return A string representing the current error for the stream
+const char *sm_error(const_stream_t stream) __attribute__((access(read_only, 1), nonnull(1), returns_nonnull, warn_unused_result));
 
 // Now time for why I wanted a stream library
 
@@ -93,58 +106,51 @@ enum stream_status sm_close(stream_t stream) __attribute__((nonnull(1)));
 /// @param stream The stream to write the number to
 /// @param u8 The 8 bit number
 /// @return The new status of the stream after the call
-enum stream_status sm_w8(stream_t stream, uint8_t u8) __attribute__((nonnull(1)));
+enum stream_status sm_w8(stream_t stream, uint8_t u8) __attribute__((access(read_write, 1), nonnull(1)));
 
 /// @brief Write a 16 bit number to the stream
 /// @param stream The stream to write the number to
 /// @param u16 The 16 bit number
 /// @return The new status of the stream after the call
-enum stream_status sm_w16(stream_t stream, uint16_t u16) __attribute__((nonnull(1)));
+enum stream_status sm_w16(stream_t stream, uint16_t u16) __attribute__((access(read_write, 1), nonnull(1)));
 
 /// @brief Write an 32 bit number to the stream
 /// @param stream The stream to write the number to
 /// @param u32 The 32 bit number
 /// @return The new status of the stream after the call
-enum stream_status sm_w32(stream_t stream, uint32_t u32) __attribute__((nonnull(1)));
+enum stream_status sm_w32(stream_t stream, uint32_t u32) __attribute__((access(read_write, 1), nonnull(1)));
 
 /// @brief Write an 64 bit number to the stream
 /// @param stream The stream to write the number to
 /// @param u64 The 64 bit number
 /// @return The new status of the stream after the call
-enum stream_status sm_w64(stream_t stream, uint64_t u64) __attribute__((nonnull(1)));
+enum stream_status sm_w64(stream_t stream, uint64_t u64) __attribute__((access(read_write, 1), nonnull(1)));
 
 /// @brief Read an 8 bit number from the stream
 /// @param stream The stream to read the number from
 /// @param u8 An out parameter for the 8 bit number
+/// @param truncated An out parameter for whether or not the read was truncated, can be nullptr
 /// @return The new status of the stream after the call
-enum stream_status sm_r8(stream_t stream, uint8_t *u8) __attribute__((access(write_only, 2), nonnull(1,2)));
+enum stream_status sm_r8(stream_t stream, uint8_t *u8, bool* truncated) __attribute__((access(read_write, 1), access(write_only, 2), access(write_only, 3), nonnull(1, 2)));
 
 /// @brief Read a 16 bit number from the stream
 /// @param stream The stream to read the number from
 /// @param u16 An out parameter for the 16 bit number
+/// @param truncated An out parameter for whether or not the read was truncated, can be nullptr
 /// @return The new status of the stream after the call
-enum stream_status sm_r16(stream_t stream, uint16_t *u16) __attribute__((access(write_only, 2), nonnull(1,2)));
+enum stream_status sm_r16(stream_t stream, uint16_t *u16, bool* truncated) __attribute__((access(read_write, 1), access(write_only, 2), access(write_only, 3), nonnull(1, 2)));
 
 /// @brief Read a 32 bit number from the stream
 /// @param stream The stream to read the number from
 /// @param u32 An out parameter for the 32 bit number
+/// @param truncated An out parameter for whether or not the read was truncated, can be nullptr
 /// @return The new status of the stream after the call
-enum stream_status sm_r32(stream_t stream, uint32_t *u32) __attribute__((access(write_only, 2), nonnull(1,2)));
+enum stream_status sm_r32(stream_t stream, uint32_t *u32, bool* truncated) __attribute__((access(read_write, 1), access(write_only, 2), access(write_only, 3), nonnull(1, 2)));
 
 /// @brief Read a 64 bit number from the stream
 /// @param stream The stream to read the number from
 /// @param u64 An out parameter for the 64 bit number
+/// @param truncated An out parameter for whether or not the read was truncated, can be nullptr
 /// @return The new status of the stream after the call
-enum stream_status sm_r64(stream_t stream, uint64_t *u64) __attribute__((access(write_only, 2), nonnull(1,2)));
+enum stream_status sm_r64(stream_t stream, uint64_t *u64, bool* truncated) __attribute__((access(read_write, 1), access(write_only, 2), access(write_only, 3), nonnull(1, 2)));
 
-/// @brief Get the current error for a stream
-/// @param stream The stream to get the error from
-/// @return A string representing the current error for the stream
-const char *stream_error(stream_t stream) __attribute__((nonnull(1)));
-
-/// @brief Check if a stream is bad (i.e. if it errored or was truncated), closed is not considered bad here.
-/// @param status The status code to check
-#define STREAM_BAD(status) ({                                \
-    typeof_unqual(status) _status = status;                  \
-    _status == STREAM_ERRORED || _status == STREAM_TRUNCATED \
-})
--- src/common/stream.c
+++ src/common/stream.c
@@ -3,7 +3,7 @@
  *  stream.c
  *  author: Lexi Allen
  *  license: MIT
- *  last updated: 9/14/2026
+ *  last updated: 9/15/2026
  *
  *  Implementation of the generic data stream library for files and memory
  *
@@ -39,36 +39,38 @@ struct file_stream
     int stored_errno;
 };
 
-static ssize_t file_read(struct stream_header *stream, void *buffer, size_t buffer_size)
+static ssize_t file_read(stream_t stream, void *buffer, size_t buffer_size)
 {
     // Assume if we get here, that the stream is asserted to be okay
     struct file_stream *fstream = (struct file_stream *)stream;
     ssize_t result = fread(buffer, 1, buffer_size, fstream->file);
-    if (result < 0)
+    if (ferror(fstream->file))
     {
         fstream->stored_errno = errno;
         stream->status = STREAM_ERRORED;
+        result = -1;
     }
-    if (feof(fstream->file))
+    else if (feof(fstream->file))
     {
         stream->status = STREAM_END;
     }
     return result;
 }
 
-static ssize_t file_write(struct stream_header *stream, void *buffer, size_t buffer_size)
+static ssize_t file_write(stream_t stream, const void *buffer, size_t buffer_size)
 {
     struct file_stream *fstream = (struct file_stream *)stream;
     ssize_t result = fwrite(buffer, 1, buffer_size, fstream->file);
-    if (result < 0)
+    if (ferror(fstream->file))
     {
         fstream->stored_errno = errno;
         stream->status = STREAM_ERRORED;
+        result = -1;
     }
     return result;
 }
 
-static void file_flush(struct stream_header *stream)
+static void file_flush(stream_t stream)
 {
     struct file_stream *fstream = (struct file_stream *)stream;
     if (fflush(fstream->file) < 0)
@@ -78,7 +80,7 @@ static void file_flush(struct stream_header *stream)
     }
 }
 
-static void file_close(struct stream_header *stream)
+static void file_close(stream_t stream)
 {
     struct file_stream *fstream = (struct file_stream *)stream;
     if (fclose(fstream->file) < 0)
@@ -88,7 +90,7 @@ static void file_close(struct stream_header *stream)
     }
 }
 
-static const char *file_error(struct stream_header *stream)
+static const char *file_error(const_stream_t stream)
 {
     struct file_stream *fstream = (struct file_stream *)stream;
     return strerror(fstream->stored_errno);
@@ -104,6 +106,8 @@ static struct stream_info file_vtable = {
 stream_t sm_file(FILE *file)
 {
     struct file_stream *stream = malloc(sizeof(struct file_stream));
+    if (stream == nullptr)
+        panic("failed to allocate file stream: %s", strerror(errno));
     stream->header.vtable = &file_vtable;
     stream->header.status = feof(file) ? STREAM_END : STREAM_OK;
     stream->file = file;
@@ -123,7 +127,7 @@ stream_t sm_file(FILE *file)
  * mw_close() - The close implementation for a memory write stream
  * mw_error() - The error implementation for a memory write stream
  * mw_vtable - The vtable for a memory write stream
- * sm_mewrite() - Create a new memory write stream
+ * sm_memwrite() - Create a new memory write stream
  *
  *****************************************************************************/
 
@@ -139,14 +143,16 @@ struct mw_stream
     bool owned;
 };
 
-static ssize_t mw_read(struct stream_header *stream, void *buffer, size_t buffer_size)
+static ssize_t mw_read(stream_t stream, void *buffer, size_t buffer_size)
 {
+    (void)buffer;
+    (void)buffer_size;
     stream->status = STREAM_ERRORED;
     ((struct mw_stream *)stream)->stored_errno = MW_ATTEMPTED_READ;
     return -1;
 }
 
-static ssize_t mw_write(struct stream_header *stream, void *buffer, size_t buffer_size)
+static ssize_t mw_write(stream_t stream, const void *buffer, size_t buffer_size)
 {
     struct mw_stream *mstream = (struct mw_stream *)stream;
 
@@ -164,26 +170,22 @@ static ssize_t mw_write(struct stream_header *stream, void *buffer, size_t buffe
         }
     }
 
-    if (memcpy(((uint8_t)*mstream->buffer) + *mstream->cursor, buffer, buffer_size) == nullptr)
-    {
-        stream->status = STREAM_ERRORED;
-        mstream->stored_errno = errno;
-        return -1;
-    }
-
+    memcpy(((uint8_t *)mstream->buffer) + *mstream->cursor, buffer, buffer_size);
+    *mstream->cursor += buffer_size;
     return (ssize_t)buffer_size;
 }
 
-static void mw_flush(struct stream_header *stream)
+static void mw_flush(stream_t stream)
 {
+    (void)stream;
 }
 
-static void mw_close(struct stream_header *stream)
+static void mw_close(stream_t stream)
 {
     free(*((struct mw_stream *)stream)->buffer);
 }
 
-static const char *mw_error(struct stream_header *stream)
+static const char *mw_error(const_stream_t stream)
 {
 
     struct mw_stream *mstream = (struct mw_stream *)stream;
@@ -207,6 +209,8 @@ static struct stream_info mw_vtable = {
 stream_t sm_memwrite(void **buffer, size_t *buffer_size)
 {
     struct mw_stream *stream = malloc(sizeof(struct mw_stream));
+    if (stream == nullptr)
+        panic("failed to allocate memwrite stream: %s", strerror(errno));
     stream->header.vtable = &mw_vtable;
     stream->header.status = STREAM_OK;
 
@@ -218,100 +222,233 @@ stream_t sm_memwrite(void **buffer, size_t *buffer_size)
     else
     {
         *buffer_size = stdc_bit_ceil(max(*buffer_size, (size_t)64));
-        *buffer = malloc(buffer_size);
+        *buffer = malloc(*buffer_size);
         if (*buffer == nullptr)
-            panic("Unable to initially create memory write stream: %s", strerror(errno));
+            panic("Unable to initially create memory write stream buffer: %s", strerror(errno));
         stream->buffer = buffer;
         stream->capacity = *buffer_size;
     }
     *buffer_size = 0;
     stream->cursor = buffer_size;
     stream->stored_errno = 0;
-    return stream;
+    return (stream_t)stream;
 }
 
 /******************************************************************************
  *
  * MEMORY READ STREAMS
  *
+ * struct mr_stream - The memory read stream state
+ * mr_read() - The read implementation for a memory read stream
+ * mr_write() - A stubbed write implementation for a memory read stream
+ *              that sets an error
+ * mr_flush() - An empty flush implementation for a memory read stream
+ * mr_close() - An empty close implementation for a memory read stream
+ * mr_error() - The error implementation for a memory read stream
+ * mr_vtable - The vtable for a memory read stream
+ * sm_memread() - Create a new memory read stream
+ *
  *****************************************************************************/
 
+struct mr_stream
+{
+    struct stream_header header;
+    const void *buffer;
+    size_t buffer_size;
+    size_t cursor;
+    bool was_written_to;
+};
 
-/// @brief Read from a stream
-/// @param stream The stream to read from
-/// @param buffer The buffer to read into
-/// @param buffer_size The amount of data to read
-/// @return The amount of data read, -1 if failed to read
-ssize_t sm_read(stream_t stream, void *buffer, size_t buffer_size);
+static ssize_t mr_read(stream_t stream, void *buffer, size_t buffer_size)
+{
+    struct mr_stream *mstream = (struct mr_stream *)stream;
+    size_t remaining = mstream->buffer_size - mstream->cursor;
+    size_t toread = min(remaining, buffer_size);
+    memcpy(buffer, ((char *)mstream->buffer) + mstream->cursor, toread);
+    mstream->cursor += toread;
+    if (mstream->cursor == mstream->buffer_size)
+    {
+        stream->status = STREAM_END;
+    }
+    return (ssize_t)toread;
+}
 
-/// @brief Write to a stream
-/// @param stream The stream to write to
-/// @param buffer The buffer to write
-/// @param buffer_size The amount of data to write
-/// @return The amount of data written, -1 if failed to write
-ssize_t sm_write(stream_t stream, void *buffer, size_t buffer_size);
+static ssize_t mr_write(stream_t stream, const void *buffer, size_t buffer_size)
+{
+    (void)buffer;
+    (void)buffer_size;
+    struct mr_stream *mstream = (struct mr_stream *)stream;
+    mstream->was_written_to = true;
+    stream->status = STREAM_ERRORED;
+    return -1;
+}
 
-/// @brief Flush a stream
-/// @param stream The stream to flush
-enum stream_status sm_flush(stream_t stream);
+static void mr_flush(stream_t stream)
+{
+    (void)stream;
+}
 
-/// @brief Close a stream
-/// @param stream The stream to close
-enum stream_status sm_close(stream_t stream);
+static void mr_close(stream_t stream)
+{
+    (void)stream;
+}
 
-// Now time for why I wanted a stream library
+static const char *mr_error(const_stream_t stream)
+{
+    struct mr_stream *mstream = (struct mr_stream *)stream;
+    return mstream->was_written_to ? "cannot write to a memread stream" : "unknown error";
+}
 
-// I wish I could make this more generic
+static struct stream_info mr_vtable = {
+    mr_read,
+    mr_write,
+    mr_flush,
+    mr_close,
+    mr_error};
 
-/// @brief Write an 8 bit number to the stream
-/// @param stream The stream to write the number to
-/// @param u8 The 8 bit number
-/// @return The new status of the stream after the call
-enum stream_status sm_w8(stream_t stream, uint8_t u8);
+stream_t sm_memread(const void *buffer, size_t buffer_size)
+{
+    struct mr_stream *stream = malloc(sizeof(struct mr_stream));
+    if (stream == nullptr)
+        panic("failed to allocate memread stream: %s", strerror(errno));
+    stream->header.vtable = &mr_vtable;
+    stream->header.status = STREAM_OK;
+    stream->buffer = buffer;
+    stream->buffer_size = buffer_size;
+    stream->cursor = 0;
+    stream->was_written_to = false;
+    return (stream_t)stream;
+}
 
-/// @brief Write a 16 bit number to the stream
-/// @param stream The stream to write the number to
-/// @param u16 The 16 bit number
-/// @return The new status of the stream after the call
-enum stream_status sm_w16(stream_t stream, uint16_t u16);
+/******************************************************************************
+ *
+ * STREAM BASIC METHODS
+ *
+ * sm_read() - Read a block of data from a stream
+ * sm_write() - Write a block of data to a stream
+ * sm_flush() - Flush a stream
+ * sm_close() - Close a stream
+ * sm_error() - Get a streams error
+ *
+ *****************************************************************************/
 
-/// @brief Write an 32 bit number to the stream
-/// @param stream The stream to write the number to
-/// @param u32 The 32 bit number
-/// @return The new status of the stream after the call
-enum stream_status sm_w32(stream_t stream, uint32_t u32);
+ssize_t sm_read(stream_t stream, void *buffer, size_t buffer_size)
+{
+    if (stream->status == STREAM_END /* Anywhere that would be truncated is also here, truncation is a virtual status*/)
+        return 0;
+    if (stream->status == STREAM_ERRORED)
+        return -1;
+    return stream->vtable->read(stream, buffer, buffer_size);
+}
 
-/// @brief Write an 64 bit number to the stream
-/// @param stream The stream to write the number to
-/// @param u64 The 64 bit number
-/// @return The new status of the stream after the call
-enum stream_status sm_w64(stream_t stream, uint64_t u64);
+ssize_t sm_write(stream_t stream, const void *buffer, size_t buffer_size)
+{
+    if (stream->status == STREAM_ERRORED)
+        return -1;
+    return stream->vtable->write(stream, buffer, buffer_size);
+}
 
-/// @brief Read an 8 bit number from the stream
-/// @param stream The stream to read the number from
-/// @param u8 An out parameter for the 8 bit number
-/// @return The new status of the stream after the call
-enum stream_status sm_r8(stream_t stream, uint8_t *u8);
+enum stream_status sm_flush(stream_t stream)
+{
+    if (stream->status == STREAM_OK)
+        stream->vtable->flush(stream);
+    return stream->status;
+}
 
-/// @brief Read a 16 bit number from the stream
-/// @param stream The stream to read the number from
-/// @param u16 An out parameter for the 16 bit number
-/// @return The new status of the stream after the call
-enum stream_status sm_r16(stream_t stream, uint16_t *u16);
+void sm_close(stream_t stream)
+{
+    stream->vtable->close(stream);
+    free(stream);
+}
 
-/// @brief Read a 32 bit number from the stream
-/// @param stream The stream to read the number from
-/// @param u32 An out parameter for the 32 bit number
-/// @return The new status of the stream after the call
-enum stream_status sm_r32(stream_t stream, uint32_t *u32);
+const char *sm_error(const_stream_t stream)
+{
+    if (stream->status == STREAM_OK)
+        return "no error";
+    if (stream->status == STREAM_END)
+        return "end of stream reached";
+    if (stream->status == STREAM_ERRORED)
+        return stream->vtable->error(stream);
+    return "unknown error";
+}
 
-/// @brief Read a 64 bit number from the stream
-/// @param stream The stream to read the number from
-/// @param u64 An out parameter for the 64 bit number
-/// @return The new status of the stream after the call
-enum stream_status sm_r64(stream_t stream, uint64_t *u64);
+/******************************************************************************
+ *
+ * STREAM SIZED READ/WRITE METHODS
+ *
+ * sm_wN() - Write N bit data to a stream
+ * sm_rN() - Read N bit data from a stream
+ *
+ *****************************************************************************/
 
-/// @brief Get the current error for a stream
-/// @param stream The stream to get the error from
-/// @return A string representing the current error for the stream
-const char *stream_error(stream_t stream);
+enum stream_status sm_w8(stream_t stream, uint8_t u8)
+{
+    (void)sm_write(stream, &u8, sizeof(uint8_t));
+    return stream->status;
+}
+
+enum stream_status sm_w16(stream_t stream, uint16_t u16)
+{
+    (void)sm_write(stream, &u16, sizeof(uint16_t));
+    return stream->status;
+}
+
+enum stream_status sm_w32(stream_t stream, uint32_t u32)
+{
+    (void)sm_write(stream, &u32, sizeof(uint32_t));
+    return stream->status;
+}
+
+enum stream_status sm_w64(stream_t stream, uint64_t u64)
+{
+    (void)sm_write(stream, &u64, sizeof(uint64_t));
+    return stream->status;
+}
+
+enum stream_status sm_r8(stream_t stream, uint8_t *u8, bool *truncated)
+{
+    if (truncated)
+        *truncated = false;
+    ssize_t read = sm_read(stream, u8, sizeof(uint8_t));
+    if (read < 0)
+        return stream->status;
+    if (read != sizeof(uint8_t) && truncated)
+        *truncated = true;
+    return stream->status;
+}
+
+enum stream_status sm_r16(stream_t stream, uint16_t *u16, bool *truncated)
+{
+    if (truncated)
+        *truncated = false;
+    ssize_t read = sm_read(stream, u16, sizeof(uint16_t));
+    if (read < 0)
+        return stream->status;
+    if (read != sizeof(uint16_t) && truncated)
+        *truncated = true;
+    return stream->status;
+}
+
+enum stream_status sm_r32(stream_t stream, uint32_t *u32, bool *truncated)
+{
+    if (truncated)
+        *truncated = false;
+    ssize_t read = sm_read(stream, u32, sizeof(uint32_t));
+    if (read < 0)
+        return stream->status;
+    if (read != sizeof(uint32_t) && truncated)
+        *truncated = true;
+    return stream->status;
+}
+
+enum stream_status sm_r64(stream_t stream, uint64_t *u64, bool *truncated)
+{
+    if (truncated)
+        *truncated = false;
+    ssize_t read = sm_read(stream, u64, sizeof(uint64_t));
+    if (read < 0)
+        return stream->status;
+    if (read != sizeof(uint64_t) && truncated)
+        *truncated = true;
+    return stream->status;
+}
